fdbadf454280cdfba505085ca6a07406b11f5ef9
Ederson <edersonvidal137@gmail.com>
2022-05-19 20:52:34 -0300

    Ajuste para não tentar gerar imagem sem textos

--- imgGenerate.py
+++ imgGenerate.py
@@ -7,7 +7,8 @@ TAMANHO = (500, 500)
 COR = ImageColor.getrgb('#d16711')
 NUMERO_POST = attNumberFromFile()
 texts = readTextsFromFile()
-# text = 
 
-for text in texts:
-    pillow_img = montTextOnImages(TAMANHO, COR, NUMERO_POST, text)
+
+if texts:
+    for text in texts:
+        pillow_img = montTextOnImages(TAMANHO, COR, NUMERO_POST, text)
