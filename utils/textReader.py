68890e7dcf7fd02d748cb70413b30bd7e46719bd
Ederson <edersonvidal137@gmail.com>
2022-05-23 19:50:11 -0300

    Realizado ajuste para marcar os arquivos lidos como já usados

--- utils/textReader.py
+++ utils/textReader.py
@@ -1,5 +1,6 @@
 from glob import glob
 from os.path import join
+from os import rename
 from utils.fileManagement import readContentFromFile, verifyFile
 
 INPUT_DIR = "./inputs/"
@@ -11,6 +12,8 @@ def readTextsFromFile():
     list_texts = []
 
     for text_file in glob(input_search_pattern):
-        list_texts.append(readContentFromFile(text_file))
+        if "_used" not in text_file:
+            list_texts.append(readContentFromFile(text_file))
+            rename(text_file, text_file + "_used")
 
     return list_texts
